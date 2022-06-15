17738e1764677ad560205b5b6fef6812cbb5fc89
manik15103053 <m15103053@gmail.com>
2022-06-15 13:26:12 +0600

    Create Relationship in Product and review

---
+++ database/factories/Model/ProductFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories\Model;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name'      => $this->faker->word(),
+            'details'   => $this->faker->paragraph(),
+            'price'     => $this->faker->numberBetween(100, 1000),
+            'stock'     =>$this->faker->randomDigit(),
+            'discount'  => $this->faker->numberBetween(2, 30)
+        ];
+    }
+}
